092bbbf9aae57b8bcb7ce2cd821b5453599aa42f
colin <ttbuge@gmail.com>
2018-11-09 22:07:17 +0800

    模板和url学习

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 # Create your views here.
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    context = {}
+    context['article_obj'] = article
+    return render(request, 'article_detail.html', context)
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'article_list.html', {"articles": articles})
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import index
+from article.views import article_detail, article_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('article/', include('article.urls')),
 ]
